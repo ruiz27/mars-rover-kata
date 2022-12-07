28857bc0d92123c65bb0a50e37b0c8fedc4f78df
saruiz <saruiz@paradigmadigital.com>
2022-12-07 14:03:53 +0100

    iterate with new tests

--- src/main/java/katas/MarsRover.java
+++ src/main/java/katas/MarsRover.java
@@ -11,14 +11,54 @@ public class MarsRover {
         int x = Integer.parseInt(position.split(",")[0]);
         int y = Integer.parseInt(position.split(",")[1]);
         String dir = position.split(",")[2];
+        Point point = new Point(x,y);
         for (int i = 0; i < listCommands.length; i++) {
             if(listCommands[i]=='f'){
-                y = y + 1;
+                point = this.moveForward(dir,x,y);
             }
             if(listCommands[i]=='b'){
-                y = y - 1;
+                point = this.moveBackward(dir,x,y);
+            }
+            if(listCommands[i]=='l'){
+                dir = "W";
+            }
+            if(listCommands[i]=='r'){
+                dir = "E";
             }
         }
-        return x+","+y+","+dir;
+
+        return point.getX()+","+ point.getY()+","+dir;
     }
+
+    private Point moveForward(String dir, int x, int y){
+        if(dir.equals("N")){
+            y = y + 1;
+        }
+        if(dir.equals("S")){
+            y = y - 1;
+        }
+        if(dir.equals("W")){
+            x = x - 1;
+        }
+        if(dir.equals("E")){
+            x = x + 1;
+        }
+        return new Point(x,y);
+    }
+    private Point moveBackward(String dir, int x, int y){
+        if(dir.equals("N")){
+            y = y - 1;
+        }
+        if(dir.equals("S")){
+            y = y + 1;
+        }
+        if(dir.equals("W")){
+            x = x - 1;
+        }
+        if(dir.equals("E")){
+            x = x + 1;
+        }
+        return new Point(x,y);
+    }
+
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -39,6 +39,60 @@ class MarsRoverTest {
         //then
         assertThat(finalPosition).isEqualTo("0,-1,N");
     }
+    @Test
+    void test_movesLeft(){
+        //given
+        String position = "0,0,N";
+        String commands = "l";
+
+        //when
+        MarsRover marsRover = new MarsRover();
+        String finalPosition = marsRover.moves(position, commands);
+
+        //then
+        assertThat(finalPosition).isEqualTo("0,0,W");
+    }
+    @Test
+    void test_movesRight(){
+        //given
+        String position = "0,0,N";
+        String commands = "r";
+
+        //when
+        MarsRover marsRover = new MarsRover();
+        String finalPosition = marsRover.moves(position, commands);
+
+        //then
+        assertThat(finalPosition).isEqualTo("0,0,E");
+    }
+    @Test
+    void test_movesLeftAndForward(){
+        //given
+        String position = "1,2,N";
+        String commands = "lf";
+
+        //when
+        MarsRover marsRover = new MarsRover();
+        String finalPosition = marsRover.moves(position, commands);
+
+        //then
+        assertThat(finalPosition).isEqualTo("0,2,W");
+    }
+    @Test
+    void test_movesLeftAndLeft(){
+        //given
+        String position = "0,0,N";
+        String commands = "ll";
+
+        //when
+        MarsRover marsRover = new MarsRover();
+        String finalPosition = marsRover.moves(position, commands);
+
+        //then
+        assertThat(finalPosition).isEqualTo("0,0,S");
+    }
+
+
 
 
 
